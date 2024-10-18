3e395b14493efb5b2da5aee7e699fa4a36db69f0
Isaac Abiodun <engrisaac1234@gmail.com>
2024-10-18 20:46:57 +0100

    Update settings.py

--- loanproject/settings.py
+++ loanproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v3gyqiw57t=zuypf%pay*oj7ql&zkqj4*a%m@8lxu7f4--_&jh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['loancalculatorapp.onrender.com']
 
 
 # Application definition
